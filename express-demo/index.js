f158b55a89eab40b95463da04022eeb7d88121ca
Bishwas KOirala <wasubisu69@gmail.com>
2024-06-21 14:52:08 +0900

    next do delete

--- express-demo/index.js
+++ express-demo/index.js
@@ -18,14 +18,11 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/courses", (req, res) => {
-  res.send(courses);
+  return res.send(courses);
 });
 
 app.post("/api/courses", (req, res) => {
-  const schema = Joi.object({
-    name: Joi.string().min(3).required(),
-  });
-  const { error } = schema.validate(req.body);
+  const { error } = validateCourse(req.body)
   if (error) return res.status(400).send(error.details[0].message);
   
   const course = {
@@ -43,16 +40,14 @@ app.put('/api/courses/:id', (req , res) => {
   const course = courses.find(c => c.id === parseInt(req.params.id)) 
   if (!course) return res.status(404).send('The course with the given ID was not found')
 
-  const schema = Joi.object({
-    name: Joi.string().min(3).required()
-  })
-  const error = schema.validate(req.body)
+  const {error} = validateCourse(req.body)
+  if (error) return res.status(400).send(error.details[0].message)
 
   //validate and return error if invalid 
-  if (error) return res.status(400).send(error.details[0].message)
-    
 
-  // update record
+    // update record
+  course.name = req.body.name
+  res.send(course)  
 })
 
 app.get("/api/courses/:id", (req, res) => {
@@ -77,3 +72,11 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
 });
+
+
+function validateCourse(course) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+  return schema.validate(course);
+}
