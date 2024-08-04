7b435b3791f3a1625ce044ec878fa2c41b8f355a
Bishwas Koirala <wasubisu69@gmail.com>
2024-08-04 09:55:20 +0900

    mongo db  : did 8.querying documents
    next do : 9.comparision query operations

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -1,37 +1,52 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-// playground  db is will be created when we hit it 
-mongoose.connect("mongodb://localhost/playground")
+// playground  db is will be created when we hit it
+mongoose
+  .connect("mongodb://localhost/playground")
   .then(() => {
-    console.log("connected to mongodb")
-  }).catch(err => console.error("mongod : db connection failed"))
-
-  // define schema to 
-
-  const courseSchema = new mongoose.Schema({
-    name : String,
-    author : String ,
-    tags :[String] ,
-    date : {type : Date , default : Date.now},
-    isPublishes : Boolean,
+    console.log("connected to mongodb");
   })
+  .catch((err) => console.error("mongod : db connection failed"));
 
-  const Course = mongoose.model("Course" , courseSchema)
-  
-  async function createCourse() {
-    const course = new Course({
-      name : "Angular course",
-      author : "Mosh",
-      tags : ["angular" , "frontend"],
-      isPublishes : true
-    })
-    const result = await  course.save()
-    console.log(result)
-    
-  }
-    async function getCourses() {
-      const courses = await Course.find({author : "Mosh"}).limit(10).sort({name : "asc"}).select({name : 1 , tags : 1 })
-      console.log(courses)
-    }
-  // createCourse()
-  getCourses()
+// define schema to
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublishes: Boolean,
+});
+
+const Course = mongoose.model("Course", courseSchema);
+
+async function createCourse() {
+  const course = new Course({
+    name: "Angular course",
+    author: "Mosh",
+    tags: ["angular", "frontend"],
+    isPublishes: true,
+  });
+  const result = await course.save();
+  console.log(result);
+}
+async function getCourses() {
+    // mongo db queries
+  // eq => equal
+  // ne => notequal
+  // gt => greater than
+  // gte => greater than or equal to
+  // lt => less than
+  // lte => less than or equal to
+  // in
+  // nin => not in
+  const courses = await Course
+    // .find({price : {$gte : 10 , $lte : 20}})
+    .find({price  : {$in : [10 , 15 , 20]}})
+    .limit(10)
+    .sort({ name: "asc" })
+    .select({ name: 1, tags: 1 });
+  console.log(courses);
+}
+// createCourse()
+getCourses();
