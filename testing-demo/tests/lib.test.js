1ce465e132a03004140aa15d9915b1de6d3b61e0
Bishwas Koirala <wasubisu69@gmail.com>
2025-03-06 16:33:20 +0900

    next => test strings

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -1,16 +1,19 @@
 const lib = require('../lib')
 
-test('absolute - should return positive if input is positive', () => { 
-  const result = lib.absolute(1)
-  expect(result).toBe(1)
-})
 
-test('absolute - should return negative if input is negative', () => { 
-  const result = lib.absolute(-1)
-  expect(result).toBe(1)
-})
-
-test('absolute - should return 0 if input is 0', () => { 
-  const result = lib.absolute(0)
-  expect(result).toBe(0)
-})
+describe('absoloute', () => {
+  it('absolute - should return positive if input is positive', () => { 
+    const result = lib.absolute(1)
+    expect(result).toBe(1)
+  })
+  
+  it('absolute - should return negative if input is negative', () => { 
+    const result = lib.absolute(-1)
+    expect(result).toBe(1)
+  })
+  
+  it('absolute - should return 0 if input is 0', () => { 
+    const result = lib.absolute(0)
+    expect(result).toBe(1)
+  })
+}) 
