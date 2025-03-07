33396c4082161795932907a1ad73be32d3298e9b
Bishwas Koirala <wasubisu69@gmail.com>
2025-03-07 12:24:40 +0900

    fix errors => next string test

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -14,6 +14,6 @@ describe('absoloute', () => {
   
   it('absolute - should return 0 if input is 0', () => { 
     const result = lib.absolute(0)
-    expect(result).toBe(1)
+    expect(result).toBe(0)
   })
 }) 
